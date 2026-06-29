3087b08dd8750d2b752f4cccb50ed31a13842b74
vixenzodj <vixcenzodj.94@gmail.com>
2026-06-29 12:59:13 +0200

    Comment out unused imports in app.py
    Comment out unused imports for pandas and pandas_ta.

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import yfinance as yf
-import pandas as pd
+#import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas_ta as ta
+#import pandas_ta as ta
 import plotly.graph_objects as go
 import plotly.express as px
 from scipy import stats
